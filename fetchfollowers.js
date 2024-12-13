b90bb30e5f44fbad701842eec0eceeee1225d6d6
leo <q474737923@gmail.com>
2024-12-13 11:16:57 +0800

    refactor(core): reorganize fetchfollowers
    Added:
      - fetchfollowers.js
      - mner_walletlist.xlsx
    
    Updated:
      - merlinswapservice.js

---
+++ fetchfollowers.js
@@ -0,0 +1,31 @@
+import { twitterClient, twitterFetchClient } from './twitterClient.js';
+import { twitter_id, twitter_user_name } from './config.js';
+import { getConnection, releaseConnection } from './db.js';
+
+async function syncFollowers() {
+    console.log(twitterFetchClient.v2);
+    const result = await twitterFetchClient.v2.followers(twitter_id);
+    console.log(result);
+    try {
+        const connection = await getConnection();
+        const [dbFollowers, ] = await connection.query("select * from twitter_follows where account_id = ?", [twitter_id]);
+        let dbfs = {};
+        for (const dbf of dbFollowers) {
+            dbfs[dbf.twitter_id] = dbf.twitter_username;
+        }
+        const [dbTwitterIds, ] = await connection.query("select twitter_id, twitter_username from users");
+        for (const dtId of dbTwitterIds) {
+            if (dbfs.hasOwnProperty[dtId.twitter_id]) {
+                continue;
+            }
+            const result = await twitterFetchClient.v2.get(`/users/${twitter_id}/followed_lists`);
+            if (result.data.following === true) {
+                await connection.query("insert into twitter_follows(account_id, account_name, twitter_username, twitter_id) values(?, ?, ?, ?);", [twitter_id, twitter_user_name, dtId.twitter_username, dtId.twitter_id]);
+            }
+        }
+        releaseConnection(releaseConnection);
+      } catch (error) {
+        releaseConnection(releaseConnection);
+        console.log(error);
+      }
+};
--- merlinswapservice.js
+++ merlinswapservice.js
@@ -55,3 +55,69 @@ async function checkStarters(addr){
 
     }
 }
+async function addMerlinswapUser(addr,inviteCode) {
+    let connection;
+    try {
+        connection = await getConnection();
+        const [resultRows,] = await connection.query("select * from merlin_swap_users where wallet_address = ?", [addr]);
+        if (resultRows.length===0){
+            const [resultRows, ] = await connection.query("select * from users where wallet_address = ?", [addr]);
+            const bindtwitterid=resultRows[0].twitter_id;
+            let steps='step1;';
+            if (bindtwitterid){
+                steps='step1;step2;';
+            }
+            if (inviteCode){
+                await connection.query("insert into merlin_swap_users(wallet_address,steps,father_code) values(?,?,?)", [addr,steps,inviteCode]);
+            }else {
+                await connection.query("insert into merlin_swap_users(wallet_address,steps) values(?,?)", [addr,steps]);
+            }
+        }
+    } finally {
+        if (connection) {
+            releaseConnection(connection)
+        }
+    }
+}
+
+async function setMerlinswapBindSteps(addr,finishSteps,newSteps) {
+    let connection;
+    try {
+        connection = await getConnection();
+        const [resultRows,] = await connection.query("select * from merlin_swap_users where wallet_address = ? and steps =? ", [addr,finishSteps]);
+        if (resultRows.length === 0) {
+            return false;
+        }
+        await connection.query("update merlin_swap_users set steps = ?  where wallet_address = ?", [finishSteps+newSteps, addr]);
+    } catch (err) {
+        console.log("merlin swap set steps error:", err)
+    } finally {
+        if (connection) {
+            releaseConnection(connection);
+        }
+    }
+}
+
+async function setMerlinswapBindStepsFinal(addr,preSteps,finalSteps) {
+    let connection;
+    try {
+        connection = await getConnection();
+        const [resultRows,] = await connection.query("select * from merlin_swap_users where wallet_address = ? and steps =? ", [addr,preSteps]);
+        if (resultRows.length === 0) {
+            return false;
+        }
+        await connection.query("update merlin_swap_users set steps = ?  where wallet_address = ?", [finalSteps, addr]);
+    } catch (err) {
+        console.log("setBindStepsFinal error:", err)
+    } finally {
+        if (connection) {
+            await releaseConnection(connection);
+        }
+    }
+}
+
+async function merlinSwapBindingTwittet(twitterId, addr) {
+    await service.bind(twitterId, addr);
+    await setMerlinswapBindSteps(addr,"step1;","step2;");
+}
+
