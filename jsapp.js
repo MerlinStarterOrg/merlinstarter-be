d4dd54ca512b562808f2724319b1fc097f80ff13
leo <q474737923@gmail.com>
2025-02-14 10:25:09 +0800

    fix(core): resolve .gitignore issues
    Added:
      - .gitignore
      - addstarts.xlsx
    
    Updated:
      - jsapp.js

--- jsapp.js
+++ jsapp.js
@@ -158,3 +158,135 @@ app.get('/claim', isAuthenticated,async (req, res) => {
       const abiCoder = AbiCoder.defaultAbiCoder();
       // placeholder
      
+      // const DOMAIN_SEPARATOR = "0xca1664004bb7b39c0fec1ed876ca84c8a46eebc1374145d86eeb2215460709da";
+      const DOMAIN_SEPARATOR = keccak256(
+          abiCoder.encode(
+              ['bytes32', 'bytes32', 'bytes32', 'uint256', 'address'],
+              [config.claimErc1155.DOMAIN_TYPEHASH,config.claimErc1155.DOMAIN, keccak256('1'), chainId, contractAddress]
+          )
+      );
+      console.log("DOMAIN_SEPARATOR::"+DOMAIN_SEPARATOR)
+  
+      // placeholder
+      const CLAIM_TYPEHASH = config.claimErc1155.CLAIM_TYPEHASH;
+      console.log("CLAIM_TYPEHASH:: "+CLAIM_TYPEHASH)
+      const params=keccak256(
+          abiCoder.encode(
+              ['bytes32', 'address', 'uint256', 'uint256', 'uint256', 'uint256'],
+              [CLAIM_TYPEHASH, message.to, message.id, message.amount, message.nonce, message.deadline]
+          )
+      )
+      const digest = keccak256(
+          ethers.solidityPacked(
+              ['string', 'bytes32', 'bytes32'],
+              [
+                  '\x19\x01',
+                  DOMAIN_SEPARATOR,
+                  params
+              ]
+          )
+      );
+      console.log("digest:: "+digest)
+ 
+      const signingKey = new ethers.SigningKey(config.claimErc1155.singer);
+      const signature = signingKey.sign(digest);
+      console.log("sinatrue:"+JSON.stringify({v:signature.v,r:signature.r,s:signature.s}))
+      // placeholder
+      const recoveredAddress=ethers.recoverAddress(digest,signature);         
+      console.log("recoveredAddress::"+recoveredAddress);
+      // placeholder
+      const claimData = claimBitAxe1155.methods.claim(message, [signature.v], [signature.r], [signature.s]).encodeABI();
+      // placeholder
+      console.log('Encoded claim function call:', claimData);
+      sendResponse(res,200,200,"success",{"claimdata":claimData,address:claimAccount,contractAddress:contractAddress});
+   } catch (error) {
+     console.error("claim bitaxe1155 error:",error)
+     sendResponse(res,500,500,"error");
+   }
+});
+
+app.get('/claim_reards',async (req, res) => {
+  try {
+    const claimAccount = req.query.address;
+    if (!claimAccount) {
+        // placeholder
+        return sendResponse(res, 400, 400, "Invalid address");
+    }
+    const web3 = new Web3(new Web3.providers.HttpProvider(config.rpc.url));
+    const contractAddress = config.claimRewards.contract;
+    const claimRewardContract = new web3.eth.Contract(claimRewardsAbi, contractAddress);
+    // placeholder
+    let nonce = await claimRewardContract.methods.nonces(claimAccount).call();
+    nonce = Number(nonce);
+    const chainId =  await web3.eth.getChainId();
+    const result = await claimReward(claimAccount, nonce);
+    if(result==0){
+        sendResponse(res, 200, 501, "no balance");
+        return;
+    }
+    const claimData = await generateClaimData(result, nonce,claimAccount, chainId,claimRewardContract);
+
+    sendResponse(res, 200, 200, "success", { "claimdata":claimData,address:claimAccount,contractAddress:contractAddress });
+} catch (error) {
+    console.error("claim reward error:", error);
+    sendResponse(res, 500, 500, "Error");
+}
+});
+
+async function generateClaimData(result,nonce, claimAccount, chainId,claimRewardContract) {
+    const amount = ethers.parseUnits(result, 18); 
+    const deadline = parseInt(Date.now() / 1000) + 3600; // placeholder
+    const message = {
+        erc20Token: config.rewardContractAddress,
+        to: claimAccount,
+        amount: amount,
+        nonce: nonce, // placeholder
+        deadline: deadline
+    };
+
+    // placeholder
+    const abiCoder = AbiCoder.defaultAbiCoder();
+
+    // placeholder
+    const DOMAIN_TYPEHASH = config.claimRewards.DOMAIN_TYPEHASH;
+    const DOMAIN_SEPARATOR = keccak256(
+        abiCoder.encode(
+            ['bytes32', 'bytes32', 'bytes32', 'uint256', 'address'],
+            [DOMAIN_TYPEHASH,config.claimRewards.DOMAIN, keccak256('1'), chainId, config.claimRewards.contract]
+        )
+    );
+
+    // placeholder
+    const CLAIM_TYPEHASH = config.claimRewards.CLAIM_TYPEHASH;
+    const digest = keccak256(
+        ethers.solidityPacked(
+            ['string', 'bytes32', 'bytes32'],
+            [
+                '\x19\x01',
+                DOMAIN_SEPARATOR,
+                keccak256(
+                    abiCoder.encode(
+                        ['bytes32', 'address', 'address', 'uint256', 'uint256', 'uint256'],
+                        [CLAIM_TYPEHASH, message.erc20Token, message.to, message.amount, message.nonce, message.deadline]
+                    )
+                )
+            ]
+        )
+    );
+
+    const signingKey = new ethers.SigningKey(config.claimRewards.singer);
+    const signature = signingKey.sign(digest);
+    console.log("sinatrue:"+JSON.stringify({v:signature.v,r:signature.r,s:signature.s}))
+    // placeholder
+    const recoveredAddress=ethers.recoverAddress(digest,signature);         
+    console.log("recoveredAddress::"+recoveredAddress);
+    // placeholder
+    const claimData = claimRewardContract.methods.claim(message, [signature.v], [signature.r], [signature.s]).encodeABI();
+
+    return claimData;
+}
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
